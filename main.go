38315b40cc9438d63d9133ec4c1b09d8855edb9a
Moushu <1924246952@qq.com>
2021-11-04 17:23:12 +0800

    commit

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
-import "awesomeProject/ch5"
+import (
+	"log"
+	"time"
+)
 
 func main() {
-	ch5.FindlinksMain()
+	bigSlowOperation()
+}
+
+func test() (i int) {
+	defer func() { i++ }()
+	return 1
+}
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")()
+
+	time.Sleep(5 * time.Second)
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit %s (%s),", msg, time.Since(start))
+	}
 
 }
